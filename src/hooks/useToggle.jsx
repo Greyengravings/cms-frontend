2a6505ba97ef8d2b24a8f37a77fd92986cad93c8
Hitesh Nihalani <nihalanihitesh13@gmail.com>
2025-10-13 00:24:24 +0530

    Updates

--- src/hooks/useToggle.jsx
+++
@@ -1,6 +0,0 @@
-import { useState } from 'react';
-export default function useToggle(initial = false) {
-  const [value, setValue] = useState(initial);
-  const toggle = () => setValue(v => !v);
-  return [value, toggle];
-}
